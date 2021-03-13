47d8a8233b389003e2dc8aff60a58cef2a511271
Bruce M <i4mbruce@hotmail.com>
2021-03-13 16:54:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let n1 = document.getElementById('n1');
 let n2 = document.getElementById('n2');
+let res = document.getElementById('resul');
 let soma = n1+n2;
 
 function calc(){
-    window.alert(n1)
-    window.alert(n2)
+    window.alert(n1.value)
+    window.alert(n2.value)
+    n3.value = Number(n1.value) + Number(n2.value)
 }
