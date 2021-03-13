3a3634eceb41925260a72d94e12cce9b646037a1
Bruce M <i4mbruce@hotmail.com>
2021-03-13 17:16:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let n1 = document.getElementById('n1');
 let n2 = document.getElementById('n2');
 let res = document.getElementById('resul');
 
-    console.log(n1.value)
-    console.log(n2.value)
-    n3.value = Number(n1.value) + Number(n2.value)
+   // console.log(n1.value)
+   // console.log(n2.value)
+    console.log(n3.value = Number(n1.value) + Number(n2.value))
 }
