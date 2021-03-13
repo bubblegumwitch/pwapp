797236ecf8e69d47f4780cc5d0e012f9248699a9
Bruce M <i4mbruce@hotmail.com>
2021-03-13 16:57:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function calc(){
-    
+
 let n1 = document.getElementById('n1');
 let n2 = document.getElementById('n2');
 let res = document.getElementById('resul');
 
-    window.alert(n1.value);
-    window.alert(n2.value);
-    n3.value = Number(n1.value) + Number(n2.value);
+    window.alert(n1.value)
+    window.alert(n2.value)
+    n3.value = Number(n1.value) + Number(n2.value)
 }
